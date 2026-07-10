83ff32c37319d106f8afa0067e5b3e6e58d8c6ea
Mark <sanamamarknoriel9@gmail.com>
2026-07-11 03:03:03 +0800

    Add InsightsPage with navigation support and data binding for interruptions and completion statistics

--- DailyTasks/Views/Pages/InsightsPage.xaml.cs
+++ DailyTasks/Views/Pages/InsightsPage.xaml.cs
@@ -1,8 +1,21 @@
 using System.Windows.Controls;
+using DailyTasks.ViewModels;
+using Wpf.Ui.Abstractions.Controls;
 
 namespace DailyTasks.Views.Pages;
 
-public partial class InsightsPage : Page
+public partial class InsightsPage : Page, INavigationAware
 {
-    public InsightsPage() => InitializeComponent();
+    private readonly InsightsViewModel _viewModel;
+
+    public InsightsPage(InsightsViewModel viewModel)
+    {
+        _viewModel = viewModel;
+        DataContext = viewModel;
+        InitializeComponent();
+    }
+
+    public Task OnNavigatedToAsync() => _viewModel.LoadAsync();
+
+    public Task OnNavigatedFromAsync() => Task.CompletedTask;
 }
