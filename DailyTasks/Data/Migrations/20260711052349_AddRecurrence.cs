d1801ef849196645cea15790b9b0f1e04a965ba2
Mark <sanamamarknoriel9@gmail.com>
2026-07-11 14:00:53 +0800

    Add Recurrence column to TaskItem and update model snapshot

---
+++ DailyTasks/Data/Migrations/20260711052349_AddRecurrence.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DailyTasks.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRecurrence : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Recurrence",
+                table: "Tasks",
+                type: "TEXT",
+                maxLength: 16,
+                nullable: false,
+                defaultValue: "None");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Recurrence",
+                table: "Tasks");
+        }
+    }
+}
