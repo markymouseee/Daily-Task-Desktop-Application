9dbbdea711f89fe259d0188d10b3c7f9d9e2596c
Mark <sanamamarknoriel9@gmail.com>
2026-07-11 21:04:53 +0800

    Add ProjectResources and update service registrations for project management

--- DailyTasks/App.xaml.cs
+++ DailyTasks/App.xaml.cs
@@ -78,9 +78,13 @@ public partial class App : Application
             options.UseSqlite(AppDbContext.ConnectionString));
 
         services.AddSingleton<ITaskService, TaskService>();
+        services.AddSingleton<IProjectService, ProjectService>();
         services.AddSingleton<ICategoryService, CategoryService>();
         services.AddSingleton<IInterruptionService, InterruptionService>();
         services.AddSingleton<ITaskEditor, TaskEditor>();
+        services.AddSingleton<ISubtaskEditor, SubtaskEditor>();
+        services.AddSingleton<IProjectExporter, ProjectExporter>();
+        services.AddSingleton<IProjectCoordinator, ProjectCoordinator>();
         services.AddSingleton<SettingsService>();
         services.AddSingleton<GlobalHotkeyService>();
         services.AddSingleton<FocusService>();
@@ -97,6 +101,8 @@ public partial class App : Application
         services.AddTransient<TodayPage>();
         services.AddTransient<AllTasksPage>();
         services.AddTransient<AllTasksViewModel>();
+        services.AddTransient<ProjectsPage>();
+        services.AddTransient<ProjectsViewModel>();
         services.AddTransient<CompletedPage>();
         services.AddTransient<CompletedViewModel>();
         services.AddTransient<InsightsPage>();
