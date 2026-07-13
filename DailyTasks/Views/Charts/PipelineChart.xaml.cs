762a1e646f911250fb39c77d2d3c8700965ec208
Mark <sanamamarknoriel9@gmail.com>
2026-07-14 01:52:20 +0800

    feat: Add PipelineChart and VShapedGanttChart for enhanced project visualization

---
+++ DailyTasks/Views/Charts/PipelineChart.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows.Controls;
+
+namespace DailyTasks.Views;
+
+/// <summary>Renders a <see cref="ViewModels.PipelineViewModel"/> as a looping DevOps pipeline.</summary>
+public partial class PipelineChart : UserControl
+{
+    public PipelineChart() => InitializeComponent();
+}
---
+++ DailyTasks/Views/Charts/VShapedGanttChart.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows.Controls;
+
+namespace DailyTasks.Views;
+
+/// <summary>Renders a <see cref="ViewModels.VShapedGanttViewModel"/> as a two-row V-Model diagram.</summary>
+public partial class VShapedGanttChart : UserControl
+{
+    public VShapedGanttChart() => InitializeComponent();
+}
